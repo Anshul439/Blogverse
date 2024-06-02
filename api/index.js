1557dfee2eb6580dae102247da4b26f6a8652704
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-06-02 21:53:35 +0530

    Create and run the server

---
+++ api/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,7 @@ import { FaMoon } from "react-icons/fa";
 import React from "react";
 
 export default function Header() {
-    const path = useLocation().pathname
+  const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2">
       <Link
@@ -33,20 +33,20 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" pill>
+          <Button gradientDuoTone="purpleToBlue" outline pill>
             Sign In
           </Button>
         </Link>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path === "/"} as={'div'}>
+        <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/about"} as={'div'}>
+        <Navbar.Link active={path === "/about"} as={"div"}>
           <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/projects"} as={'div'}>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
           <Link to="/projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
