5001b6bbca46d10229e742da0def83192a0106e5
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-06-15 19:21:03 +0530

    Complete sidebar of the dashboard

---
+++ client/src/components/DashProfile.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DashProfile() {
+  return (
+    <div>DashProfile</div>
+  )
+}
--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -5,5 +5,5 @@ import Dashboard from "../pages/Dashboard";
 export const PrivateRoute = () => {
   const { currentUser } = useSelector((state) => state.user);
   console.log(currentUser);
-  return currentUser ? <Outlet/> : <Navigate to="/sign-in" />;
+  return currentUser ? <Dashboard/> : <Navigate to="/sign-in" />;
 };
