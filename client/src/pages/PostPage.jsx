3d41f89c52f25436961b684176d93877a1f65969
Anshul Wadhwa <anshulwadhwa439@gmail.com>
2024-08-13 00:37:08 +0530

    Add recent article section to post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,13 +1,16 @@
-import { Button, Spinner } from "flowbite-react";
-import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import CommentSection from "../components/CommentSection";
+
+import { Button, Spinner } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import CommentSection from '../components/CommentSection';
+import PostCard from '../components/PostCard';
 
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -20,9 +23,11 @@ export default function PostPage() {
           setLoading(false);
           return;
         }
-        setPost(data.posts[0]);
-        setError(false);
-        setLoading(false);
+        if (res.ok) {
+          setPost(data.posts[0]);
+          setLoading(false);
+          setError(false);
+        }
       } catch (error) {
         setError(true);
         setLoading(false);
@@ -32,52 +37,63 @@ export default function PostPage() {
   }, [postSlug]);
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [postSlug]);
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
 
   if (loading)
     return (
-      <div className="flex justify-center items-center min-h-screen">
-        <Spinner size="xl" />
+      <div className='flex justify-center items-center min-h-screen'>
+        <Spinner size='xl' />
       </div>
     );
-
-  if (error)
-    return (
-      <div className="flex justify-center items-center min-h-screen">
-        <p>Error loading post.</p>
-      </div>
-    );
-
   return (
-    <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
-      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+    <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
+      <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>
         {post && post.title}
       </h1>
       <Link
         to={`/search?category=${post && post.category}`}
-        className="self-center mt-5"
+        className='self-center mt-5'
       >
-        <Button color="gray" pill size="xs">
+        <Button color='gray' pill size='xs'>
           {post && post.category}
         </Button>
       </Link>
       <img
         src={post && post.image}
         alt={post && post.title}
-        className="mt-10 p-3 max-h-[600px] w-full object-cover"
+        className='mt-10 p-3 max-h-[600px] w-full object-cover'
       />
-      <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-3xl text-xs">
+      <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
-        <span className="italic">
-          {post && (post.content.length / 1500).toFixed(0)} mins read
+        <span className='italic'>
+          {post && (post.content.length / 1000).toFixed(0)} mins read
         </span>
       </div>
       <div
-        className="p-3 max-w-3xl mx-auto w-full post-content"
+        className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
       <CommentSection postId={post._id} />
+
+      <div className='flex flex-col justify-center items-center mb-5'>
+        <h1 className='text-xl mt-5'>Recent articles</h1>
+        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+      </div>
     </main>
   );
 }
