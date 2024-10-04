4012ccba881d8e07fb72a08fa501d06317253dfb
Shahbaz Ali <aquibahmad345@gmail.com>
2024-10-04 16:19:15 +0530

    Test auth api's and some bug fixes

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema(
 );
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModifies("password")) return next();
+  if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 10);
   next();
